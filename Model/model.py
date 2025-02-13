8351bdc9dccf81dc1bdadd53955970d9d40816e5
Harsh-Tiwadi <yo_harsh@outlook.com>
2025-02-13 23:54:37 +0530

    Model

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,3 @@
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, r2_score, classification_report, ConfusionMatrixDisplay
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
